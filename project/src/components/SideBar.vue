a536d7c15292fc084b135575cea4c1c4b78e0c3f
Gobluebro <griffinstrayer@gmail.com>
2021-01-13 22:34:33 -0500

    Creating Header and Sidebar components

---
+++ project/src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      images: [],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
