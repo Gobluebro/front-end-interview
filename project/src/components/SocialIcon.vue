1f0757a29f826c0f984a0573ad64c4258ca22d4c
Gobluebro <griffinstrayer@gmail.com>
2021-01-14 23:13:55 -0500

    Component to handle making svg icons based on data

---
+++ project/src/components/SocialIcon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="">
+    <img
+      v-bind:src="IconSetter(socials.id)"
+      v-bind:href="socials.link"
+      v-bind:alt="socials.type"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["socials"],
+  data: function() {
+    return {
+      src: "",
+    };
+  },
+  methods: {
+    IconSetter: function(id) {
+      switch (id) {
+        case 0:
+          this.src = "../assets/icons/social-pinterest.svg";
+          break;
+        case 1:
+          this.src = "../assets/icons/social-twitter.svg";
+          break;
+        case 2:
+          this.src = "../assets/icons/social-instagram.svg";
+          break;
+        case 3:
+          this.src = "../assets/icons/social-youtube.svg";
+          break;
+        default:
+          break;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+svg {
+  fill: white;
+}
+</style>
