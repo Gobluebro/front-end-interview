6a75da39b94a2f2e297115d07c58ce19ffadc4c1
Gobluebro <griffinstrayer@gmail.com>
2021-01-17 00:41:17 -0500

    Commiting to fallback on if I change my mind
    Going to use flex box instead of grid.

--- project/src/components/Header.vue
+++ project/src/components/Header.vue
@@ -1,13 +1,14 @@
 <template>
-  <header class="bg-gray-500 text-white text-2xl flex">
+  <header
+    class="bg-gray-500 text-white text-xl grid grid-rows-3 grid-flow-col gap-2 justify-start items-center"
+  >
     <img
       src="../assets/images/miranda-anderson/profile-image.jpg"
       alt="A Photo of Miranda Anderson"
-    />
-    <div class="flex-col">
-      <span>{{ firstName }} {{lastName}}</span>
-      <span>{{ blogName }}</span>
-    </div>
+      class="row-span-3 mx-8 my-4"
+    /> 
+    <span class="col-span-2 text-gray-400">{{ firstName }} {{ lastName }}</span>
+    <span class="row-span-2 font-light">{{ blogName }}</span>
   </header>
 </template>
 
@@ -27,5 +28,8 @@ img {
   border-radius: 50%;
   width: 100px;
   height: 100px;
+}
+header {
+  margin-top: -60px;
 }
 </style>
