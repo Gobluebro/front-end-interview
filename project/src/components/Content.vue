2a0c41cf3ad9483668e61fd8dec523abd66e5410
Gobluebro <griffinstrayer@gmail.com>
2021-01-17 22:33:31 -0500

    Removed background color and put in main component.

--- project/src/components/Content.vue
+++ project/src/components/Content.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-gray-700 text-white p-2">
+  <main class="text-white p-2">
     <span class="text-2xl">STORY COLLECTION</span>
     <br />
     <span class="text-gray-500 text-lg">All stores by {{ firstName }} {{ lastName }}</span>
