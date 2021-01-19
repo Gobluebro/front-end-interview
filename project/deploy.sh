cecdb39a55fe4b4fb2085751a163252f71a67de0
Gobluebro <griffinstrayer@gmail.com>
2021-01-19 00:06:57 -0500

    Moving package.json and other stuff to correct project location

--- project/deploy.sh
+++ project/deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run build
+npx vue-cli-service build
 
 # navigate into the build output directory
-cd project/dist
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
